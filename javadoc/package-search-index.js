af750068c31fafa5e0f7035bfa59bac95a889e67
Catherine Camier <catherine@camier.de>
2022-11-18 17:48:31 +0100

    add javadoc

---
+++ javadoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"main.java.graphreduction.centrality"},{"l":"main.java.graphreduction.communitydetection"},{"l":"main.java.graphreduction.core"},{"l":"main.java.graphreduction.crawler"}]
